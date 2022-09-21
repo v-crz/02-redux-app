c66e3e17da5d3784137adbd81c3a189c420103c8
viry <vbatten10@hotmail.com>
2022-09-21 18:07:25 -0500

    Create reducer

--- src/app/contador/contador.reducer.ts
+++ src/app/contador/contador.reducer.ts
@@ -1,13 +1,10 @@
-import { Action } from "@ngrx/store";
+import { createReducer, on } from "@ngrx/store";
 import { decrementar, incrementar } from "./contador.actions";
 
-export function contadorReducer( state: number = 10, action: Action ){
-    switch (action.type) {
-        case incrementar.type:
-            return state + 1;
-        case decrementar.type:
-            return state - 1;
-        default:
-            return state;
-    }
-}
+export const initialState = 20;
+
+export const contadorReducer = createReducer(
+  initialState,
+  on(incrementar, (state) => state + 1),
+  on(decrementar, (state) => state - 1)
+);
